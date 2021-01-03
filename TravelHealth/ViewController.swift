09ed2a7d5993e445040e40b4eb09d108e3a44748
maitreebain <maitreebain@gmail.com>
2021-01-02 23:56:50 -0500

    travel health app

---
+++ TravelHealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelHealth
+//
+//  Created by Maitree Bain on 1/2/21.
+//  Copyright © 2021 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
