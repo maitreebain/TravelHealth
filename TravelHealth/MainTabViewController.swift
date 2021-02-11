a3991073ff83c28cd997b4ca189c2a93bcc0895a
Brandon Williams <mbrandonw@hey.com>
2021-02-11 10:19:31 -0800

    Refactor API client

--- TravelHealth/MainTabViewController.swift
+++ TravelHealth/MainTabViewController.swift
@@ -12,7 +12,6 @@ class MainTabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
 
--- TravelHealth/PlacesSection/PlacesViewController.swift
+++ TravelHealth/PlacesSection/PlacesViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class PlacesViewController: UIViewController {
+    let apiClient: APIClient
+
+    init(apiClient: APIClient = .live) {
+        self.apiClient = apiClient
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
-    
-    
 }
-
-
